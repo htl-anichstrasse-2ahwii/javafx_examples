d7d4ba9eaed237a005422c6fb80a843163eb4503
albertgreinoecker <albert.greinoecker@gmail.com>
2020-04-29 17:24:12 +0200

    removed unused imports

--- javafx_examples/src/application/ex08_accessdatabase/Ex08_AccessDatabase.java
+++ javafx_examples/src/application/ex08_accessdatabase/Ex08_AccessDatabase.java
@@ -6,8 +6,6 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.BorderPane;
