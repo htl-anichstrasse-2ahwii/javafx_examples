28d9413edc23e7855392f15f6e017c60380c61d3
albertgreinoecker <albert.greinoecker@gmail.com>
2020-05-11 13:03:54 +0200

    removed unused import

--- javafx_examples/src/application/Ex15_WorkingWithDate.java
+++ javafx_examples/src/application/Ex15_WorkingWithDate.java
@@ -9,7 +9,6 @@ import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
