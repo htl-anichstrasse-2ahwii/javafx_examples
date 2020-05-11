b03fc5c8b02cebc5774058829efc5abc87984876
albertgreinoecker <albert.greinoecker@gmail.com>
2020-05-11 10:20:49 +0200

    added general comment

--- javafx_examples/src/application/Ex14_Drawing.java
+++ javafx_examples/src/application/Ex14_Drawing.java
@@ -1,6 +1,5 @@
 package application;
 
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -10,33 +9,44 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class Ex14_Drawing extends Application{
+/**
+ * Dieses Beispiel soll zeigen, wie man mit JavaFX zeichnen kann. Von einfachen
+ * Linien, geometrischen Objekten bis zum Einbinden von Bildern ist alles
+ * moeglich.
+ * 
+ * Es ist natuerlich moeglich, dass die Zeichenflaeche (Canvas) nicht wie in
+ * diesem Beispiel das komplette Fenster ausfuellt, sondern nur Teile. Das
+ * Canvas-Element kann man naemlich gleich wie jedes Steuerelement einfach auf
+ * der Seite positionieren
+ */
+public class Ex14_Drawing extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		BorderPane root = new BorderPane();
-		Canvas canvas = new Canvas(1000, 600); //Eine Zeichenflaeche wird erzeugt
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.setFill(Color.GREEN); //Es muss immer eine Fuellfarbe gesetzt werden, bevor eine fill-Funktion verwendet wird
-        gc.fillRect(100, 100, 100, 100); //x,y,w,h
-        
-        gc.setFill(Color.RED);
-        gc.fillRect(150, 150, 100, 100);
-        
-        gc.fillOval(300, 300, 100, 100); ////x,y,w,h    w = h => Kreis
-        
-        gc.setLineWidth(3);  //Einstellen der Stiftdicke
-        gc.setStroke(Color.BLUE); //Einstellen der Zeichenfarbe
-        gc.strokeOval(500, 300, 50, 40); //Hier wird nur gezeichnet, nicht ausgefuellt
-        
-        //Zeichne ein Bild an die enstprechende Stelle
-        gc.drawImage(new Image("images/bart.png"), 500, 300);
-        
-        root.getChildren().add(canvas);
-        primaryStage.setScene(new Scene(root,1000,600));
-        primaryStage.show();
+		Canvas canvas = new Canvas(1000, 600); // Eine Zeichenflaeche wird erzeugt
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.setFill(Color.GREEN); // Es muss immer eine Fuellfarbe gesetzt werden, bevor eine fill-Funktion
+									// verwendet wird
+		gc.fillRect(100, 100, 100, 100); // x,y,w,h
+
+		gc.setFill(Color.RED);
+		gc.fillRect(150, 150, 100, 100);
+
+		gc.fillOval(300, 300, 100, 100); //// x,y,w,h w = h => Kreis
+
+		gc.setLineWidth(3); // Einstellen der Stiftdicke
+		gc.setStroke(Color.BLUE); // Einstellen der Zeichenfarbe
+		gc.strokeOval(500, 300, 50, 40); // Hier wird nur gezeichnet, nicht ausgefuellt
+
+		// Zeichne ein Bild an die enstprechende Stelle
+		gc.drawImage(new Image("images/bart.png"), 500, 300);
+
+		root.getChildren().add(canvas);
+		primaryStage.setScene(new Scene(root, 1000, 600));
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
