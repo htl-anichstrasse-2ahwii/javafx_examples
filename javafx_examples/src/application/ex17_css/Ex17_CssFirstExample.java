edc993005be22ef174568200fac2421fb73ccee8
albertgreinoecker <albert.greinoecker@gmail.com>
2020-05-17 21:24:14 +0200

    first general example for css

---
+++ javafx_examples/src/application/ex17_css/Ex17_CssFirstExample.java
@@ -0,0 +1,52 @@
+package application.ex17_css;
+
+import java.util.Collections;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Ex17_CssFirstExample extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+
+		BorderPane root = new BorderPane();
+
+		Button b = new Button("Ein Button");
+		// So wird der Style direkt gesetzt (eher nicht zu empfehlen: Darstellung soll
+		// von den Steuerelementen getrennt abgelegt werden
+		b.setStyle("-fx-font: normal bold 20px 'serif' ");
+		root.setCenter(b);
+
+		// so kann man die aktuell zugewiesenen Styles abfragen
+		String style = b.getStyle();
+		System.out.println(style);
+		
+		//so die zugewiesenen styleklassen
+		String classes = String.join(",", b.getStyleClass());
+		System.out.println(classes);
+
+		//Wenn man die id setzt, kann man speziell nur für dieses Steuerelement styles setzen:
+		b.setId("first_button");
+		
+		
+		// System.out.println(b.getStyleClass());
+		Scene scene = new Scene(root, 400, 400);
+		// Man kann mehrere Stylesheets zuweisen, es geht z.B. ein allgemeines, wo
+		// grundsätzlich festgelegt wird, wie Steuerelemente darstellt werden sollen
+		scene.getStylesheets().add("application/application.css");
+		// zusaetzlich zu speziellen Styles
+		scene.getStylesheets().add("application/ex17_css/first.css");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
