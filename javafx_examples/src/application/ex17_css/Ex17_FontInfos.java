b9f972ae437d1e82ef608d3dc7d04e0a90fbbb20
albertgreinoecker <albert.greinoecker@gmail.com>
2020-06-29 19:14:38 +0200

    Find out which fonts (for a certain family) and font families do exist. set fonts for root

---
+++ javafx_examples/src/application/ex17_css/Ex17_FontInfos.java
@@ -0,0 +1,53 @@
+package application.ex17_css;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Ex17_FontInfos extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+
+		BorderPane root = new BorderPane();
+
+		
+		
+		VBox center = new VBox();
+		Label fontNames = new Label("Font names");
+		TextArea l = new TextArea();
+		l.setEditable(false);
+		
+		for (String font: Font.getFontNames("monospace"))
+		{
+			l.appendText(font + "\n");
+		}
+
+		Label fontFamilies = new Label("Font families");
+		TextArea f = new TextArea();
+		l.setEditable(false);
+		
+		for (String familiy: Font.getFamilies())
+		{
+			f.appendText(familiy + "\n");
+		}
+		
+		fontFamilies.setStyle("-fx-font: 12px Regular");
+		//fontFamilies.setStyle("-fx-font: 100px Tahoma");
+		center.getChildren().addAll(fontNames, l, fontFamilies, f);
+		root.setCenter(center);
+		Scene scene = new Scene(root, 400, 400);
+		scene.getStylesheets().add("application/ex17_css/fonts.css");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
