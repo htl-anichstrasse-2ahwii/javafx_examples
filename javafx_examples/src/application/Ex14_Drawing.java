e2303a4cdc61a12d2cb8c73c03426d192b30f244
Albert Greinoecker <albert.greinoecker@gmail.com>
2021-05-31 09:03:04 +0200

    remove unused import

--- javafx_examples/src/application/Ex14_Drawing.java
+++ javafx_examples/src/application/Ex14_Drawing.java
@@ -1,7 +1,6 @@
 package application;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
